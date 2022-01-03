74912e45d9b511363b9b7b63ea43dd5f96361a22
eric <howcrazys@gmail.com>
2022-01-03 08:29:28 +0800

    feat: 需驗證信箱

--- app/Http/Controllers/controls/PageController.php
+++ app/Http/Controllers/controls/PageController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\controls;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PageController extends Controller
 {
     public function home()
     {
+        if( !Auth::user()->is_admin ){
+            return redirect('/')->withErrors('沒有權限');
+        }
         return view('controls.home');
     }
 }
